e23322072e71b8f0bdb6ae551fc973cfdce523aa
Archana Potnis <potnis.archana@gmail.com>
2021-07-07 10:06:05 -0700

    Added Fetch API

---
+++ searchPodcastFetch.js
@@ -0,0 +1,17 @@
+async function fetchPodcasts() {
+  const response = await fetch("podcast.json");
+  let podcasts = await response.json();
+
+  podcasts["podcasts"].map((podcast) => {
+    document.querySelector(".podcast-middle-container").innerHTML += `
+    <div id="card">
+    <img class="card-image" src="${podcast.url}" />
+    <h3>${podcast.title}</h3>
+    <h4>${podcast.genre}</h4>
+    <div class="btn-listen">Listen</div>
+    </div>
+      `;
+  });
+}
+
+fetchPodcasts();
